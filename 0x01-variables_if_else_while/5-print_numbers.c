f47c9bc407f0b6984d753023b18cc603b470438f
Tahir Adeleye <98001661+Tahir-DA-DS@users.noreply.github.com>
2022-04-09 12:54:12 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
+@@ -10,8 +11,9 @@ int main(void)
+	char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is positive\n", n);
-
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		}
+	putchar('\n');
 	return (0);
 }
